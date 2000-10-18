9df8a12967b5d46db66ed028e3bd0150191cf8e5
Morten Eriksen <mortene@sim.no>
2000-10-18 11:52:39 +0000

    Added a simple test program for checking the image loaders.

---
+++ tests/loaders.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <simage.h>
+
+int
+main(int argc, char ** argv)
+{
+  if (argc < 2) {
+    (void)fprintf(stderr, "\n\tUsage: %s IMGFILE [IMGFILES]\n\n", argv[0]);
+    exit(1);
+  }
+
+  for (int i=1; i < argc; i++) {
+    const char * filename = argv[i];
+    if (!simage_check_supported(filename)) {
+      (void)fprintf(stdout, "``%s'' is not in a supported format\n", filename);
+    }
+    else {
+      (void)fprintf(stdout, "``%s'':\n", filename);
+
+      int w, h, comp;
+      unsigned char * buffer = simage_read_image(filename, &w, &h, &comp);
+      if (!buffer) {
+	char errbuffer[256];
+	(void)fprintf(stdout, "\t** couldn't load file: \"%s\"\n",
+		      simage_get_last_error(errbuffer, 255));
+      }
+      else {
+	(void)fprintf(stdout, "\twidth: %d, height: %d, components: %d\n",
+		      w, h, comp);
+	free(buffer);
+      }
+    }
+    (void)fprintf(stdout, "\n");
+  }
+
+  // FIXME: should write testcode for the plugin API functions
+  // aswell. 20001018 mortene.
+
+  return 0;
+}
