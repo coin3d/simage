fb2072f5ee40f3d9e0d2e029e0f143195cfa3ec2
Morten Eriksen <mortene@sim.no>
2000-10-19 14:08:42 +0000

    Testcode was supposed to be a C file.

--- tests/loaders.c
+++ tests/loaders.c
@@ -5,21 +5,24 @@
 int
 main(int argc, char ** argv)
 {
+  int i;
+
   if (argc < 2) {
     (void)fprintf(stderr, "\n\tUsage: %s IMGFILE [IMGFILES]\n\n", argv[0]);
     exit(1);
   }
 
-  for (int i=1; i < argc; i++) {
+  for (i=1; i < argc; i++) {
     const char * filename = argv[i];
     if (!simage_check_supported(filename)) {
       (void)fprintf(stdout, "``%s'' is not in a supported format\n", filename);
     }
     else {
-      (void)fprintf(stdout, "``%s'':\n", filename);
-
       int w, h, comp;
       unsigned char * buffer = simage_read_image(filename, &w, &h, &comp);
+
+      (void)fprintf(stdout, "``%s'':\n", filename);
+
       if (!buffer) {
 	(void)fprintf(stdout, "\t** couldn't load file: \"%s\"\n",
 		      simage_get_last_error());
@@ -27,14 +30,14 @@ main(int argc, char ** argv)
       else {
 	(void)fprintf(stdout, "\twidth: %d, height: %d, components: %d\n",
 		      w, h, comp);
-  	free(buffer);
+  	simage_free_image(buffer);
       }
     }
     (void)fprintf(stdout, "\n");
   }
 
-  // FIXME: should write testcode for the plugin API functions
-  // aswell. 20001018 mortene.
+  /* FIXME: should write testcode for the plugin API functions
+     aswell. 20001018 mortene. */
 
   return 0;
 }
