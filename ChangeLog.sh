bfc2dc1e9b1e6869bc94e522b34e7f6c48541595
Morten Eriksen <mortene@sim.no>
2000-10-21 17:42:48 +0000

    Ignore changes in cfg/m4/ (renamed from conf-macros).

--- ChangeLog.sh
+++ ChangeLog.sh
@@ -17,7 +17,7 @@ This is the ChangeLog file for the $module module.  This file is automatically
 generated every night.  Entries are in reversed chronological order.  Check
 also out the following ChangeLog file:
 
-  ./conf-macros/ChangeLog
+  ./cfg/m4/ChangeLog
 
 See http://www.red-bean.com/~kfogel/cvs2cl.shtml for information about the
 cvs2cl script used to generate this file.
@@ -34,7 +34,7 @@ cvs log -d">= 1 January 2000" | $cvs2cl --stdin --header $headerfile --separate-
   --ignore '(Makefile\.in|configure|aclocal\.m4|config\.sub|config\.guess)$' \
   --ignore '(ltconfig|ltmain\.sh|missing|mkinstalldirs|stamp-h.*|install-sh)$' \
   --ignore 'config\.h\.in$' \
-  --ignore 'conf-macros/'
+  --ignore 'cfg/m4'
 
 rm ChangeLog.bak $headerfile
 
