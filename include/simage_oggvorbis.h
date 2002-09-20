4fa7522d260503a6d8cfe919f3da590d4e80169b
Thomas Hammer <thammer@sim.no>
2002-09-20 12:06:40 +0000

    One more file for oggvorbis support.

---
+++ include/simage_oggvorbis.h
@@ -0,0 +1,16 @@
+#ifndef SIMAGE_OGGVORBIS_H
+#define SIMAGE_OGGVORBIS_H
+
+#include <config.h>
+
+#ifndef SIMAGE_OGGVORBIS_SUPPORT
+#error "This file should not be used under the current configuration!"
+#endif /* !SIMAGE_OGGVORBIS_READER_SUPPORT */
+
+#include <simage.h>
+
+int oggvorbis_reader_stream_open(const char * filename, s_stream * stream);
+int oggvorbis_reader_stream_get(s_stream * stream, void * buffer, int *size, s_params * params);
+void oggvorbis_reader_stream_close(s_stream * stream);
+
+#endif /* SIMAGE_OGGVORBIS_H */
