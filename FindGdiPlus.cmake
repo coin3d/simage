1939c31819f5b93483981fd73132787bdd0c3310
Markus Rickert <rickert@fortiss.org>
2017-05-14 16:42:46 +0200

    Find GdiPlus and Vfw outside of developer command prompt using registry keys

--- FindGdiPlus.cmake
+++ FindGdiPlus.cmake
@@ -1,10 +1,30 @@
 include(FindPackageHandleStandardArgs)
 
-find_path(GDIPLUS_INCLUDE_DIRS NAMES gdiplus.h)
+find_path(
+	GDIPLUS_INCLUDE_DIRS
+	NAMES
+	GdiPlus.h gdiplus.h
+	HINTS
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows\\v7.0;InstallationFolder]"
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows\\v7.0A;InstallationFolder]"
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows\\v7.1;InstallationFolder]"
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows\\v7.1A;InstallationFolder]"
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows\\v8.0;InstallationFolder]"
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows\\v8.0A;InstallationFolder]"
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows\\v8.1;InstallationFolder]"
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows\\v8.1A;InstallationFolder]/Include/um"
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows Kits\\Installed Roots;KitsRoot]"
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows Kits\\Installed Roots;KitsRoot81]"
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows Kits\\Installed Roots;KitsRoot10]"
+	PATH_SUFFIXES
+	Include
+	Include/um
+	Include/shared
+)
 
 mark_as_advanced(GDIPLUS_INCLUDE_DIRS)
 
-find_library(GDIPLUS_LIBRARIES NAMES gdiplus)
+set(GDIPLUS_LIBRARIES gdiplus)
 
 find_package_handle_standard_args(
 	GdiPlus
--- FindVfw.cmake
+++ FindVfw.cmake
@@ -1,10 +1,31 @@
 include(FindPackageHandleStandardArgs)
 
 find_path(VFW_INCLUDE_DIRS NAMES vfw.h)
+find_path(
+	VFW_INCLUDE_DIRS
+	NAMES
+	Vfw.h vfw.h
+	HINTS
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows\\v7.0;InstallationFolder]"
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows\\v7.0A;InstallationFolder]"
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows\\v7.1;InstallationFolder]"
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows\\v7.1A;InstallationFolder]"
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows\\v8.0;InstallationFolder]"
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows\\v8.0A;InstallationFolder]"
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows\\v8.1;InstallationFolder]"
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows\\v8.1A;InstallationFolder]/Include/um"
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows Kits\\Installed Roots;KitsRoot]"
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows Kits\\Installed Roots;KitsRoot81]"
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows Kits\\Installed Roots;KitsRoot10]"
+	PATH_SUFFIXES
+	Include
+	Include/um
+	Include/shared
+)
 
 mark_as_advanced(VFW_INCLUDE_DIRS)
 
-find_library(VFW_LIBRARIES NAMES vfw32)
+set(VFW_LIBRARIES vfw32)
 
 find_package_handle_standard_args(
 	Vfw
